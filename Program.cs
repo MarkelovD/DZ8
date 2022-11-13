e86410a827183cc84572bb8f71a59ab006cc922a
Dmitry <markelov16@gmail.com>
2022-11-13 23:17:19 +0500

    2 задача

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ PrintArray(array); //печать массива
 Console.WriteLine("");
 // Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит 
 //по убыванию элементы каждой строки двумерного массива.
-SortingArray(array);
+//SortingArray(array);
 // вывод массива
 void PrintArray(int[,] Array)
 {
@@ -36,24 +36,51 @@ int[,] GetArray(int m, int n, int minValue, int maxValue)
     }
     return result; //возвращение заполненного массива
 }
-void SortingArray(int[,] Array)
+// void SortingArray(int[,] Array)
+// {
+//     for (int i = 0; i < Array.GetLength(0); i++)//проход по строкам
+//     { // заполнение строк (0)
+//         for (int j = 0; j < Array.GetLength(1); j++)// проход по столбцам
+//         { // заполнение столбцов (1)
+//             for (int k = j + 1; k < Array.GetLength(1); k++)// цикл для сравнения значений относительно предыдущего элемента
+//             {
+//                 if (Array[i, j] < Array[i, k])
+//                 {
+//                     int temp = Array[i, j];//пузырьковая сортировка
+//                     Array[i, j] = Array[i, k];
+//                     Array[i, k] = temp;
+//                     continue;
+//                 }
+//             }
+//             Console.Write($"{Array[i, j]} "); // вывод заполненого массива
+//         }
+//         Console.WriteLine(); // переход но новую строку массива
+//     }
+// }
+//Задача 56: Задайте прямоугольный двумерный массив. 
+//Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+MaxSumm(array);
+//метод суммы чисел в строках 2 задача
+void MaxSumm(int[,] Array)
 {
-    for (int i = 0; i < Array.GetLength(0); i++)//проход по строкам
-    { // заполнение строк (0)
-        for (int j = 0; j < Array.GetLength(1); j++)// проход по столбцам
-        { // заполнение столбцов (1)
-            for (int k = j + 1; k < Array.GetLength(1); k++)// цикл для сравнения значений относительно предыдущего элемента
-            {
-                if (Array[i, j] < Array[i, k])
-                {
-                    int temp = Array[i, j];//пузырьковая сортировка
-                    Array[i, j] = Array[i, k];
-                    Array[i, k] = temp;
-                    continue;
-                }
-            }
-            Console.Write($"{Array[i, j]} "); // вывод заполненого массива
-        }
-        Console.WriteLine(); // переход но новую строку массива
+    int StartSumm = 0;// переменная для нахождения суммы элементов массива 0 строки
+    int pozition = 0;//переменная для обозначения нужной строки
+    for (int i = 0; i < array.GetLength(1); i++)//цикл для нахождения суммы 0 строки
+    {
+        StartSumm += array[0, i];
     }
+    for (int j = 1; j < array.GetLength(0); j++)//цикл перебора сумм строк и
+    {
+        int sum = 0;//делаем обнуление на кадой строке
+        for (int k = 0; k < array.GetLength(1); k++)
+        {
+            sum += array[j, k];
+        }
+        if (sum > StartSumm)
+        {
+            StartSumm = sum;
+            pozition = j;
+        }
+    }
+    Console.WriteLine($"максимальная сумма элементов строки массива ={StartSumm} индекс строки №{pozition}");
 }
